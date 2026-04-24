b7d1313433de50b4c0416911a5971804373c2b20
sky <6abotalal@gmail.com>
2026-04-24 21:58:03 +0300

    Create city_rules.js

---
+++ city_rules.js
@@ -0,0 +1,14 @@
+const cityRulesData = {
+    'city-main': {
+        title: "🏙️ قـوانين الـمديـنة",
+        text: `
+            <div class="grid-container">
+                <div class="card sub-card" onclick="showContent('perm-band')">حالات الباند النهائي </div>
+                <div class="card sub-card" onclick="showContent('terms')">تعريف المصطلحات </div>
+                <div class="card sub-card" onclick="showContent('chat-rules')">قوانين شات اللعبة </div>
+                <div class="card sub-card" onclick="showContent('admin-req')">طلب الإداريين </div>
+                <div class="card sub-card" onclick="showContent('kidnap-rules')">الخطف والتلويت </div>
+                <div class="card sub-card" onclick="showContent('general-city')">قوانين عامة </div>
+            </div>`
+    }
+};
