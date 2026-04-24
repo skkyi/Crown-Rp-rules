27f3651d29387f37e9e52e9b3c945b2591f5e973
sky <6abotalal@gmail.com>
2026-04-24 21:58:40 +0300

    Create ministry_rules.js

---
+++ ministry_rules.js
@@ -0,0 +1,14 @@
+const ministryRulesData = {
+    'ministry-main': {
+        title: "👑 قـوانين الـوزارات",
+        text: `
+            <div class="grid-container">
+                <div class="card sub-card" onclick="showContent('civil-system')">⚖️ النظام المدني</div>
+                <div class="card sub-card" onclick="showContent('min-system')">📋 النظام الوزاري</div>
+                <div class="card sub-card" onclick="showContent('state-const')">📜 دستور الدولة</div>
+                <div class="card sub-card" onclick="showContent('immunity')">🛡️ نظام الحصانات</div>
+                <div class="card sub-card" onclick="showContent('public-security')">👮🏻 مديرية الأمن العام</div>
+                <div class="card sub-card" onclick="showContent('royal-guard')">👑 الحرس الملكي</div>
+            </div>`
+    }
+};
