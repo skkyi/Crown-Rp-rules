28a61bed2b96b4041fda4982d75968fc930f0b47
sky <6abotalal@gmail.com>
2026-04-24 23:00:23 +0300

    Update main_menu.js

--- main_menu.js
+++ main_menu.js
@@ -1,12 +1,12 @@
 const mainMenuData = {
     'main': {
-        title: "💎 لوحة قوانين مدينة كراون",
+        title: "👑 قوانين مدينة كراون 👑",
         text: `
             <div class="grid-container">
-                <div class="card main-card" onclick="loadCityRules()">🏙️ قـوانين الـمديـنة ⤥</div>
-                <div class="card main-card" onclick="loadGangRules()">🔪 قوانين المنظمات والعصابات ⤥</div>
-                <div class="card main-card" onclick="loadDiscordRules()">📱 قـوانين الـدسكورد ⤥</div>
-                <div class="card main-card" onclick="loadMinistryRules()">👑 قـوانين الـوزارات ⤥</div>
+                <div class="card main-card" onclick="loadCityRules()"> قـوانين الـمديـنة ✦</div>
+                <div class="card main-card" onclick="loadGangRules()"> قوانين المنظمات والعصابات ✦</div>
+                <div class="card main-card" onclick="loadDiscordRules()"> قـوانين الـدسكورد ✦</div>
+                <div class="card main-card" onclick="loadMinistryRules()"> قـوانين الـوزارات ✦</div>
             </div>`
     }
 };
